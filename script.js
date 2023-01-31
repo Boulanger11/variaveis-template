1f30d05944dbe1584ea19d10e752a92c07259ffb
Boulanger11 <leo.tcheco@gmail.com>
2023-01-31 19:09:35 -0300

    exercicio variaveis e tipos primitivos

--- script.js
+++ script.js
@@ -1,1 +1,84 @@
+// 1. COMENTÁRIO
+// ISSO É UM COMENTÁRIO
+
+/*
+EU GOSTO DE PEIXE
+E GATO
+*/
+
+// 2. CONSOLE.LOG
+//console.log("Olá, mundo!")
+//console.log("oiiiii")
+
+// 3. PROMPT
+//prompt("Qual o seu nome?")
+
+// 4. PRIMEIRA PRATICA
+//const nome = "Leonardo Boulanger"
+//const nome = prompt("Qual é o seu nome?")
+//console.log(nome)
+//console.log(typeof nome)
+
+//let idade = Number(prompt("Qual é a sua idade?"))
+
+//console.log(idade)
+//console.log(typeof idade)
+
+
+
+//idade = 55
+//console.log(idade)
+
+//console.log("Olá!", "Meu nome é", nome, "e eu tenho", idade, "anos.")
+//console.log("Quando eu tinha", idade, "anos", "conheci o", nome)
+
+// Segunda prática 
+
+//cenário a
+//temPet = false
+//nomePet = null
+
+// cenário b
+//let temPet = true
+//const nomePet = "Ingrid"
+
+//console.log("Tipo da variável nome", typeof nome)
+//console.log("Tipo da variável nome", typeof idade)
+//console.log("Tipo da variável nome", typeof temPet)
+
+//TESTEconst numero1 = Number(prompt(diga um numero))
+//const numero2 = Number(prompt(Diga outro numero))
+
+//const resultado = numero1 + numero2
+//console.log (resultado)
+
+//EXERCICIO 1
+//const nome = "garrafa"
+//console.log(nome)
+//console.log("tipo da variavel nome",typeof nome)
+//let numero = 1
+//console.log(numero)
+//console.log("tipo da variavel nome",typeof numero)
+//let metal = false
+//console.log(metal)
+//console.log("tipo da variavel nome",typeof metal)
+
+//console.log("Eu escolhi o objeto", nome, "que tem", numero, "no quarto,mas é de metal", metal)
+
+//EXERCICIO 2
+let a = 24
+let b = 29
+let c  
+
+//GUARDANDO VALOR c
+c = a
+console.log("C:", c)
+
+//GUARDANDO VALOR a
+a = b
+console.log("A:", a)
+
+//GUARDANDO VALOR b
+b = c
+console.log("B:", b)
 
